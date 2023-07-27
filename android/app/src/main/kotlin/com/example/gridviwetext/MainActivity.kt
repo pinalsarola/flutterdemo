5c0bdda19dad450bc00cfd74bf629ac8e7432734
pinalsarola <pinalsarola28@gmail.com>
2023-07-27 14:11:48 +0530

    First commit

---
+++ android/app/src/main/kotlin/com/example/gridviwetext/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.gridviwetext
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/modalclass.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+class Data{
+  String image;
+  String text;
+  String $item;
+  Data ({required this.image,required this.text,required this.$item});
+
+}
